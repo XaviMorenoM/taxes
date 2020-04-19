5b8cb5a12103b1479cadb9f885dfef1ca273a03f
Xavi Moreno <xavier.moreno.martinez@gmail.com>
2020-04-19 23:00:19 +0200

    throw error on failing exception

--- src/db/manager.ts
+++ src/db/manager.ts
@@ -2,10 +2,14 @@ import mongoose from 'mongoose'
 import payer from './models/payer'
 
 export default () => {
-	mongoose.connect('mongodb://localhost:27017', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
+	mongoose
+		.connect('mongodb://localhost:27017', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		.catch(err => {
+			throw new Error(err.message)
+		})
 
 	return {
 		models: { payer }
